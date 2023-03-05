bd10c6f41964f9c55b139ed0d7ddd14eddf96d0c
Ronnie Santos <115759491+RonnieVip@users.noreply.github.com>
2023-03-04 21:00:18 -0300

    Add files via upload
    Calcula o preço por quilo de uma ração.

---
+++ ração_preço_kg.php
@@ -0,0 +1,49 @@
+  <!DOCTYPE html>
+<html>
+    <head>
+        <title>Preço ração por quilo</title>
+        
+    </head>
+    <body>
+        <form action="" method="POST">
+            <table align="center">
+                <tr>
+                    <td>Preço:</td>
+                    <td><input type="text" name="preco"></td>
+                </tr>
+                <tr>
+                    <td>Desconto:</td>
+                    <td><input type="text" name="desconto"></td>
+                </tr>        
+                <tr>
+                    <td>Quilo:</td>
+                    <td><input type="text" name="quilo"></td>
+                </tr>                                    
+                <tr>
+                    <td></td>
+                    <td><input type="submit" name="enviar" value="Enviar"></td>
+                </tr>
+            </table>
+        </form>
+        
+    </body>
+</html>
+
+<?php
+
+if (isset($_POST["enviar"]))
+{    
+
+    $preco  = $_POST["preco"];
+    $desconto = $_POST["desconto"];
+    $quilo = $_POST["quilo"];
+    $preco_quilo = ($preco*$desconto)/$quilo;   
+
+  
+    echo "<br>Preço por quilo: ".$preco_quilo."<br>";   
+
+    echo "<br>";
+
+   
+}
+?>
